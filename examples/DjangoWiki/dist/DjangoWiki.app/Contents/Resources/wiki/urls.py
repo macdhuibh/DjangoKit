4d07ec8c4319cd0e580c41fe7ee425a7d20bef18
Anders Hofstee <a.hofstee@catalpainternational.org>
2012-04-16 14:00:00 +0900

    In models.py maxlength through an error. Changed to max_length.

--- examples/DjangoWiki/dist/DjangoWiki.app/Contents/Resources/wiki/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import patterns
-
-urlpatterns = patterns('wiki.views',
-    (r'^((?:[A-Z]+[a-z]+){2,})/$', 'page'),
-    (r'^edit/((?:[A-Z]+[a-z]+){2,})/$', 'edit'),
-    (r'^delete/$', 'delete'),
-    (r'^$', 'index'),
-)
- 
--- examples/DjangoWiki/wiki/models.py
+++ examples/DjangoWiki/wiki/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.conf import settings
 
+
 class Wikipage(models.Model):
     """Wiki page storage"""
-    title = models.CharField(maxlength=30)
+    title = models.CharField(max_length=30)
     content = models.TextField()
 
     def editurl(self):
