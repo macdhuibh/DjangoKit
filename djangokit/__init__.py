bfc66391673fde60e7c5c726d3e6f03cbf3c1fe6
Anders Hofstee <a.hofstee@catalpainternational.org>
2012-04-16 13:50:38 +0900

    import syncdb failed so changed to call_command('syncdb') and pep8 goodness

--- djangokit/__init__.py
+++ djangokit/__init__.py
@@ -1,5 +1,5 @@
 from distutils.core import setup as setup_core
-import py2app,sys,os
+import py2app, sys, os
 from glob import glob
 import django
 from AppKit import NSBundle
@@ -9,52 +9,53 @@ from AppKit import NSBundle
 # _works_, and it wouldn't if I got sidetracked by every little thing
 # I didn't understand. Patches welcome.. :-)
 
+
 def setup(**args):
 
     appname = args['appname']
     del args['appname']
-    
+
     if 'prettyname' in args:
         prettyname = args['prettyname']
         del args['prettyname']
     else:
         prettyname = appname
-    
+
     if 'settings' in args:
         more_settings = args['settings']
         del args['settings']
     else:
         more_settings = {}
-    
+
     NSBundle.mainBundle().infoDictionary()[u'DjangoKit'] = {
-      'appname':appname,
-      'prettyname':prettyname,
+      'appname': appname,
+      'prettyname': prettyname,
     };
-    
+
     os.environ['DJANGO_SETTINGS_MODULE'] = 'djangokit.settings'
-    from django.core.management import syncdb
+    from django.core.management import call_command
     from django.conf import settings
-    settings.DATABASE_NAME # reading a property inflated the settings object
+    settings.DATABASE_NAME  # reading a property inflated the settings object
     settings.DATABASE_NAME = "database.sqlite"
-    syncdb()
-    
+    call_command('syncdb')
+
     plist = dict(
         NSMainNibFile="MainMenu",
-        CFBundleName = prettyname,
-        CFBundleIdentifier="org.jerakeen.DjangoKitApps.%s"%appname, # TODO - could be better
-        CFBundleShortVersionString = args['version'],
-        CFBundleVersion = args['version'],
-        NSHumanReadableCopyright="Copyright 2007 %s"%args['author'],
+        CFBundleName=prettyname,
+        CFBundleIdentifier="org.jerakeen.DjangoKitApps.%s" % appname,  # TODO - could be better
+        CFBundleShortVersionString=args['version'],
+        CFBundleVersion=args['version'],
+        NSHumanReadableCopyright="Copyright 2007 %s" % args['author'],
         DjangoKit={
-          'appname':appname,
-          'prettyname':prettyname,
-          'settings':more_settings,
+          'appname': appname,
+          'prettyname': prettyname,
+          'settings': more_settings,
         },
     )
-    
+
     py2app_options = dict(
         plist=plist,
-        packages = [appname, 'djangokit', 'django'],
+        packages=[appname, 'djangokit', 'django'],
     )
 
     if 'iconfile' in args:
@@ -62,10 +63,10 @@ def setup(**args):
         del args['iconfile']
 
     base = __import__('djangokit').__path__[0]
-    nibfile = "%s/MainMenu.nib"%base
-    setup_core( **dict( args,
-        app=[ "%s/app.py"%base ],
+    nibfile = "%s/MainMenu.nib" % base
+    setup_core(**dict(args,
+        app=["%s/app.py" % base],
         # TODO - this requires a local 'media' folder. Don't.
-        data_files = ['media', nibfile, 'database.sqlite', appname],
+        data_files=['media', nibfile, 'database.sqlite', appname],
         options=dict(py2app=py2app_options),
     ))
