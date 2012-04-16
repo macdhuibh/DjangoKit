2950de750a80ea2dab7cf2b266fc1d445ca5ce8a
Anders Hofstee <a.hofstee@catalpainternational.org>
2012-04-16 14:00:00 +0900

    In models.py maxlength through an error. Changed to max_length.

--- examples/todo/todo/models.py
+++ examples/todo/todo/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
+
 class Todo(models.Model):
-    date = models.DateTimeField('date created', auto_now_add = True)
-    value = models.CharField(maxlength = 200)
+    date = models.DateTimeField('date created', auto_now_add=True)
+    value = models.CharField(max_length=200)
     completed = models.BooleanField()
-    
+
     class Admin:
 
-        list_display = [ 'date', 'value', 'completed' ]
-        
+        list_display = ['date', 'value', 'completed']
+
         fields = [
-            [ None, { 'fields':[ 'date', 'value', 'completed' ] } ],
+            [None, {'fields': ['date', 'value', 'completed']}],
         ]
-        
-        list_filter = [ 'completed' ]
-        search_fields = [ 'value' ]
+
+        list_filter = ['completed']
+        search_fields = ['value']
